7ea97a30ef70807a9d6413a576c15be684738fb3
Éderson Fernandes <ederson.fernandes@bempromotora.onmicrosoft.com>
2023-01-18 13:51:02 -0300

    update

--- unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
+++ unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
@@ -7,17 +7,12 @@ namespace Prime.Services
         public bool IsPrime(int candidate)
         {
             if (candidate < 2)
-            {
                 return false;
-            }
 
             for (var divisor = 2; divisor <= Math.Sqrt(candidate); divisor++)
-            {
                 if (candidate % divisor == 0)
-                {
                     return false;
-                }
-            }
+
             return true;
         }
     }
